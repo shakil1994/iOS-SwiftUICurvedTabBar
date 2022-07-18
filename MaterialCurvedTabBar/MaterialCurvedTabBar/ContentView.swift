ec2044b8d61d8faccaedac8534d931bd967bfd43
shakil1994 <shakil.sub.cse@gmail.com>
2022-07-18 14:31:15 +0600

    Add Project

---
+++ MaterialCurvedTabBar/MaterialCurvedTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MaterialCurvedTabBar
+//
+//  Created by Md Shah Alam on 7/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MaterialCurvedTabBar/MaterialCurvedTabBar/MaterialCurvedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialCurvedTabBarApp.swift
+//  MaterialCurvedTabBar
+//
+//  Created by Md Shah Alam on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MaterialCurvedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
